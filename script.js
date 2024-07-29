b4aeab45e5fb366091f99028f40df364e16abd33
Gabriel <bielbettib@gmail.com>
2024-07-29 16:36:38 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,7 @@
+function calculaArea(base, altura) {
+    return base * altura;
+}
+    let base = 5;
+    let altura = 10;
+    let area = calculaArea(base, altura);
+    console.log("A área é: " + area);
